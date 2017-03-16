08d21d43fdcadd2dd46a009a273383ccfc859411
Brahian Giraldo Medina <brahian609@hotmail.com>
2017-03-16 13:53:16 -0500

    Se ajusto funcionalidad de la ventana chat

--- js/socket.js
+++ js/socket.js
@@ -10,14 +10,36 @@ socket.on('connect', () => {
         email: email
       };
     socket.emit('new_chat', data);
+    $("#contact").hide();
+    $('#message').show();
   });
 
-  $('#send_message').click(function () {
-    let message = $("#message").val(),
-      data = {
-        message: message
-      };
-    socket.emit('send_message', data);
-    $("#message").val("");
+  $('#message').on('keyup', function (event) {
+    if (event.which === 13) {
+      let message = $(this).val(),
+        date = new Date(),
+        hour = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+        data = {
+          message: message
+        };
+      socket.emit('send_message', data);
+
+      let $chat = $("#chat");
+      $chat.show();
+
+      $chat.append(`
+        <div class="row msg_container base_sent">
+            <div class="col-xs-10 col-md-10">
+                <div class="messages msg_sent">
+                    <p>${message}</p>
+                    <time datetime="2009-11-13T20:00">${hour}</time>
+                </div>
+            </div>
+        </div>
+      `);
+
+      $(this).val('');
+      return false;
+    }
   });
 });
