12495e8bac3b52092372b394973f5b03b0c3f6b1
Brahian Giraldo Medina <brahian609@hotmail.com>
2017-03-16 19:12:57 -0500

    Refactoriza codigo

--- js/socket.js
+++ js/socket.js
@@ -1,18 +1,35 @@
-let socket = io('http://localhost:3001');
+let socket = io('http://192.168.81.32:3001');
 
 socket.on('connect', () => {
   console.log('connect');
   $('#send').click(function () {
     let name = $("#name").val(),
       email = $("#email").val(),
+      message = $("#message_contact").val(),
+      date = new Date(),
+      hour = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
       data = {
         name: name,
         email: email
       };
     socket.emit('new_chat', data, (response) => {
-      if (response){
+      if (response) {
         $("#contact").hide();
         $('#message').show();
+
+        let $chat = $("#chat");
+        $chat.show();
+
+        $chat.append(`
+          <div class="row msg_container base_sent">
+              <div class="col-xs-10 col-md-10">
+                  <div class="messages msg_sent">
+                      <p>${message}</p>
+                      <time datetime="2009-11-13T20:00">${hour}</time>
+                  </div>
+              </div>
+          </div>
+        `);
       }
     });
   });
@@ -40,9 +57,25 @@ socket.on('connect', () => {
             </div>
         </div>
       `);
-
       $(this).val('');
       return false;
     }
+  });
+
+  socket.on('receiveMessage', (data) => {
+    let $chat = $("#chat"),
+      date = new Date(),
+      hour = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+    $chat.append(`
+       <div class="row msg_container base_receive">
+          <div class="col-xs-10 col-md-10">
+              <div class="messages msg_receive">
+                  <p>${data.message}</p>
+                  <time datetime="2009-11-13T20:00">${hour}</time>
+              </div>
+          </div>
+       </div>
+     `);
   });
 });
